e3afa69bf9c60cb730ad649f7cc1991f0c00c832
websmithcode <websmithcode@gmail.com>
2024-12-02 13:14:46 +0300

    Add build client bundle

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,8 @@ declare module "@remix-run/node" {
 export default defineConfig({
   plugins: [
     remix({
+      ssr: false,
+
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
